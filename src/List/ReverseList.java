97883891713b61c2aa675911b6dacd45f98cdd39
WHeLiosC <2469660964@qq.com>
2022-02-24 17:11:49 +0800

    Add ReverseList

---
+++ src/List/ReverseList.java
@@ -0,0 +1,36 @@
+package List;
+
+/**
+ * @author lihui
+ */
+public class ReverseList {
+    /**
+     * 206. 反转链表
+     *
+     * @param head 单链表的头节点 head
+     * @return 反转后的链表
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while (cur != null) {
+            tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2, n1);
+        ListNode n3 = new ListNode(3, n2);
+        ListNode n4 = new ListNode(4, n3);
+        ListNode n5 = new ListNode(5, n4);
+
+        ReverseList r = new ReverseList();
+        r.reverseList(n5);
+    }
+}
