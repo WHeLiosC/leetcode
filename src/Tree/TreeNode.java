c975ad7a11b1a9d268db2ed0278a3cbe3414cb31
WHeLiosC <2469660964@qq.com>
2022-03-07 22:13:28 +0800

    add TreeNode

---
+++ src/Tree/TreeNode.java
@@ -0,0 +1,26 @@
+package Tree;
+
+/**
+ * Definition for a binary tree node.
+ *
+ * @author lihui
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
