7255e8d28f3b82a7f579abeb37353ae5d9e24f26
WHeLiosC <2469660964@qq.com>
2022-03-08 17:36:26 +0800

    update Traversal

--- src/Tree/Traversal.java
+++ src/Tree/Traversal.java
@@ -265,13 +265,14 @@ public class Traversal {
         return result;
     }
 
+    private List<Integer> rightSideViewResult = new LinkedList<>();
+
     /**
      * 199. 二叉树的右视图 (DFS)
      *
      * @param root 根节点
      * @return 给定一个二叉树的根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值
      */
-    private List<Integer> rightSideViewResult = new LinkedList<>();
     public List<Integer> rightSideViewDFS(TreeNode root) {
         rightSideViewRecursion(root, 0);
         return rightSideViewResult;
@@ -287,5 +288,96 @@ public class Traversal {
         ++depth;
         rightSideViewRecursion(node.right, depth);
         rightSideViewRecursion(node.left, depth);
+    }
+
+    /**
+     * 637. 二叉树的层平均值
+     *
+     * @param root 根节点
+     * @return 以数组的形式返回每一层节点的平均值
+     */
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            double sum = 0;
+            for (int i = 0; i < len; i++) {
+                TreeNode tempNode = queue.poll();
+                sum += tempNode.val;
+                if (tempNode.left != null) {
+                    queue.offer(tempNode.left);
+                }
+                if (tempNode.right != null) {
+                    queue.offer(tempNode.right);
+                }
+            }
+            result.add(sum / len);
+        }
+        return result;
+    }
+
+    /**
+     * 515. 在每个树行中找最大值
+     *
+     * @param root 根节点
+     * @return 找出该二叉树中每一层的最大值
+     */
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            int maxNumInLevel = Integer.MIN_VALUE;
+            for (int i = 0; i < len; i++) {
+                TreeNode tempNode = queue.poll();
+                if (tempNode.val > maxNumInLevel) {
+                    maxNumInLevel = tempNode.val;
+                }
+                if (tempNode.left != null) {
+                    queue.offer(tempNode.left);
+                }
+                if (tempNode.right != null) {
+                    queue.offer(tempNode.right);
+                }
+            }
+            result.add(maxNumInLevel);
+        }
+        return result;
+    }
+
+    /**
+     * 104. 二叉树的最大深度
+     *
+     * @param root 根节点
+     * @return 返回其最大深度，根节点所在深度为 1
+     */
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    /**
+     * 111. 二叉树的最小深度
+     *
+     * @param root 根节点
+     * @return 返回其最小深度，根节点所在深度为 1
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMinDepth = minDepth(root.left);
+        int rightMinDepth = minDepth(root.right);
+        // 如果左子树或右子树的深度不为 0，即存在一个子树，那么当前子树的最小深度就是该子树的深度+1
+        // 如果左子树和右子树的深度都不为 0，即左右子树都存在，那么当前子树的最小深度就是它们较小值+1
+        return (leftMinDepth == 0 || rightMinDepth == 0) ? leftMinDepth + rightMinDepth + 1 : Math.min(leftMinDepth, rightMinDepth) + 1;
     }
 }
