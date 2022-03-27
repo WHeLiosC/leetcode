498133b3b35de161e6f60147bc14c1fddd4e6686
WHeLiosC <2469660964@qq.com>
2022-03-27 15:42:20 +0800

    add CanPartition

---
+++ src/DynamicProgramming/CanPartition.java
@@ -0,0 +1,15 @@
+package DynamicProgramming;
+
+/**
+ * @author lihui
+ */
+public class CanPartition {
+    /**
+     * 416. 分割等和子集
+     * @param nums 只包含正整数 的 非空 数组 nums
+     * @return 判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等
+     */
+    public boolean canPartition(int[] nums) {
+        return true;
+    }
+}
