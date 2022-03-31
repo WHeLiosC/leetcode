caf0aad1d331557de211a37d5f51401542a9ed98
WHeLiosC <2469660964@qq.com>
2022-03-31 09:04:58 +0800

    update ClimbStairs

--- src/DynamicProgramming/ClimbStairs.java
+++ src/DynamicProgramming/ClimbStairs.java
@@ -25,6 +25,27 @@ public class ClimbStairs {
     }
 
     /**
+     * 70. 爬楼梯 (改编)
+     *
+     * @param n 楼梯阶数
+     * @return 每次你可以爬 最多 m 个台阶。返回有多少种不同的方法可以爬到楼顶
+     */
+    public int climbStairsPro(int n, int m) {
+        // 相当于有 1~m 个物品，每个物品有无数件，填满容量为 n 的背包
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        // 方案数是一个排列数，所以先遍历背包，再遍历物品
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i >= j) {
+                    dp[i] += dp[i - j];
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    /**
      * 746. 使用最小花费爬楼梯
      *
      * @param cost 整数数组，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用
