27f28e3ec8bc6268a8e3f6dc10ab91edde4b093f
WHeLiosC <2469660964@qq.com>
2022-04-01 17:43:49 +0800

    make field public

--- src/Tree/TreeNode.java
+++ src/Tree/TreeNode.java
@@ -6,9 +6,9 @@ package Tree;
  * @author lihui
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode() {
 
