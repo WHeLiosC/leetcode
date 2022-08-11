a5fea2f096d67da5a497ea01ea0a52e9300bba5b
WHeLiosC <2469660964@qq.com>
2022-08-11 21:17:51 +0800

    modify MergeTwoLists

--- src/List/MergeTwoLists.java
+++ src/List/MergeTwoLists.java
@@ -14,23 +14,20 @@ public class MergeTwoLists {
      * @return 合并后的新的升序链表
      */
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode pointer1 = list1;
-        ListNode pointer2 = list2;
         ListNode dummyHead = new ListNode();
-        dummyHead.next = list1;
         ListNode pre = dummyHead;
-        while (pointer1 != null && pointer2 != null) {
-            if (pointer1.val <= pointer2.val) {
-                pre.next = pointer1;
-                pointer1 = pointer1.next;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                pre.next = list1;
+                list1 = list1.next;
             } else {
-                pre.next = pointer2;
-                pointer2 = pointer2.next;
+                pre.next = list2;
+                list2 = list2.next;
             }
             pre = pre.next;
         }
 
-        pre.next = pointer1 == null ? pointer2 : pointer1;
+        pre.next = list1 == null ? list2 : list1;
 
         return dummyHead.next;
     }
